f037fd60dd2869c4a4713553d546742532db150e
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2024-04-20 15:03:54 -0400

    a

--- grab.py
+++ grab.py
@@ -1,6 +1,7 @@
 import http.client, json
 
-conn = http.client.HTTPSConnection("ptb.discord.com")
+#region Main discord
+conn = http.client.HTTPSConnection("discord.com")
 
 conn.request("GET", "/api/v9/applications/detectable")
 
@@ -8,3 +9,26 @@ res = conn.getresponse()
 data = res.read()
 jdata = json.loads(data.decode())
 odata = json.dump(jdata,indent=4,fp=open("detectable.json",'w'))
+#endregion
+
+#region PTB discord
+conn = http.client.HTTPSConnection("ptb.discord.com")
+
+conn.request("GET", "/api/v9/applications/detectable")
+
+res = conn.getresponse()
+data = res.read()
+jdata = json.loads(data.decode())
+odata = json.dump(jdata,indent=4,fp=open("ptb detectable.json",'w'))
+#endregion
+
+#region Canary discord
+conn = http.client.HTTPSConnection("canary.discord.com")
+
+conn.request("GET", "/api/v9/applications/detectable")
+
+res = conn.getresponse()
+data = res.read()
+jdata = json.loads(data.decode())
+odata = json.dump(jdata,indent=4,fp=open("canary detectable.json",'w'))
+#endregion
