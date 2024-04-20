58995a5242f67765a81cc87c1fdb1374c27b1e19
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2024-04-20 14:52:20 -0400

    push

---
+++ grab.py
@@ -0,0 +1,10 @@
+import http.client, json
+
+conn = http.client.HTTPSConnection("discord.com")
+
+conn.request("GET", "/api/v9/applications/detectable")
+
+res = conn.getresponse()
+data = res.read()
+jdata = json.loads(data.decode())
+odata = json.dump(jdata,indent=4,fp=open("detectable.json",'w'))
