11dc019ee58196a2c8c2a865cf6a048bf104b349
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2024-05-05 18:22:04 -0700

    Update grab.py

--- grab.py
+++ grab.py
@@ -1,34 +1,78 @@
 import http.client, json
+Main:bool = True
+PTB:bool = True
+Canary:bool = True
 
 #region Main discord
-conn = http.client.HTTPSConnection("discord.com")
+if Main:
+    with open("json/detectable.json",'r+') as file:
+        conn = http.client.HTTPSConnection("discord.com")
 
-conn.request("GET", "/api/v9/applications/detectable")
+        conn.request("GET", "/api/v9/applications/detectable")
 
-res = conn.getresponse()
-data = res.read()
-jdata = json.loads(data.decode())
-odata = json.dump(jdata,indent=4,fp=open("json/detectable.json",'w'))
+        res = conn.getresponse()
+        data = res.read()
+
+        old_data = json.load(file)
+        new_data = json.loads(data.decode())
+
+        old_names = [x['name'] for x in old_data]
+        new_names = [x['name'] for x in new_data]
+        
+        # Print new names that are not in the old list
+        print("[Main]",f"{len(old_data)} -> {len(new_data)} games (+{len(new_data) - len(old_data)})")
+        print("[Main] New games:",[name for name in new_names if name not in old_names])
+
+        file.seek(0)
+        json.dump(new_data, indent=4, fp=file)
+        file.truncate()
 #endregion
 
 #region PTB discord
-conn = http.client.HTTPSConnection("ptb.discord.com")
+if PTB:
+    with open("json/ptb detectable.json",'r+') as file:
+        conn = http.client.HTTPSConnection("ptb.discord.com")
 
-conn.request("GET", "/api/v9/applications/detectable")
+        conn.request("GET", "/api/v9/applications/detectable")
 
-res = conn.getresponse()
-data = res.read()
-jdata = json.loads(data.decode())
-odata = json.dump(jdata,indent=4,fp=open("json/ptb detectable.json",'w'))
+        res = conn.getresponse()
+        data = res.read()
+
+        old_data = json.load(file)
+        new_data = json.loads(data.decode())
+
+        old_names = [x['name'] for x in old_data]
+        new_names = [x['name'] for x in new_data]
+
+        print("[PTB]",f"{len(old_data)} -> {len(new_data)} games (+{len(new_data) - len(old_data)})")
+        print("[PTB] New games:",[name for name in new_names if name not in old_names])
+
+        file.seek(0)
+        json.dump(new_data, indent=4, fp=file)
+        file.truncate()
 #endregion
 
 #region Canary discord
-conn = http.client.HTTPSConnection("canary.discord.com")
+if Canary:
+    with open("json/canary detectable.json",'r+') as file:
+        conn = http.client.HTTPSConnection("canary.discord.com")
 
-conn.request("GET", "/api/v9/applications/detectable")
+        conn.request("GET", "/api/v9/applications/detectable")
 
-res = conn.getresponse()
-data = res.read()
-jdata = json.loads(data.decode())
-odata = json.dump(jdata,indent=4,fp=open("json/canary detectable.json",'w'))
+        res = conn.getresponse()
+        data = res.read()
+
+        old_data = json.load(file)
+        new_data = json.loads(data.decode())
+
+        old_names = [x['name'] for x in old_data]
+        new_names = [x['name'] for x in new_data]
+        
+        # Print new names that are not in the old list
+        print("[Canary]", f"{len(old_data)} -> {len(new_data)} games (+{len(new_data) - len(old_data)})")
+        print("[Canary] New games:", [name for name in new_names if name not in old_names])
+
+        file.seek(0)
+        json.dump(new_data, indent=4, fp=file)
+        file.truncate()
 #endregion
