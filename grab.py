b0ff2f49f1ddcc0caa1bb48c4f4fa961750405c4
Roblox Thot <67937010+Roblox-Thot@users.noreply.github.com>
2024-10-06 06:03:46 -0400

    :prage: work

--- grab.py
+++ grab.py
@@ -16,13 +16,21 @@ with open("json/detectable.json",'r+') as file:
     new_names = [x['name'] for x in new_data]
     new_execs = [x['executables'] for x in new_data if "executables" in x]
 
+    new_names.sort()
+    new_execs.sort()
+    names_text_list = ('\n').join(new_names)
+    execs_text_list = ('\n').join(new_execs)
+    open('data/games.txt','w').write(names_text_list)
+    open('data/executables.txt','w').write(execs_text_list)
+
     # Print new names that are not in the old list
     added_games = [name for name in new_names if name not in old_names]
     added_execs = [name for name in new_execs if name not in old_execs]
     print("[Main]",f"{len(old_data)} -> {len(new_data)} games (+{len(new_data) - len(old_data)})")
     print("[Main] New games:",added_games)
     print("[Main] New executables:",len(added_execs))
-    
+
+    # Makes a temp file used for the commit messages
     commmit_msg = '(Apps update) Updated JSONs\n'
     for game in added_games:
         commmit_msg += f'- {game}\n'
@@ -36,5 +44,6 @@ with open("json/detectable.json",'r+') as file:
     exe_list = [y["name"] for x in new_execs for y in x]
     cleaned_names = sorted(set(new_names))
     cleaned_execs = sorted(set(exe_list))
+
     open('json/games.json','w').write(json.dumps(cleaned_names, indent=4))
     open('json/executables.json','w').write(json.dumps(cleaned_execs, indent=4))
